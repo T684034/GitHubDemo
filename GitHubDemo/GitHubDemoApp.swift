82d1533841a20101d487805582630ada047d719a
T684034 <alexandre.bevilacqua@ubs.com>
2021-12-27 21:50:11 +0100

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Alexandre Bevilacqua on 27.12.21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
