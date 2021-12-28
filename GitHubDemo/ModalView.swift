b056026c1374e91928763d3da1a2da7d1061ce6c
T684034 <alexandre.bevilacqua@ubs.com>
2021-12-28 12:22:10 +0100

    testing commit

---
+++ GitHubDemo/ModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ModalView.swift
+//  GitHubDemo
+//
+//  Created by Alexandre Bevilacqua on 28.12.21.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text("This is a modal view!")
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
